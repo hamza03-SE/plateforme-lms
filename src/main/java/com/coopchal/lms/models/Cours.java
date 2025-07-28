052bf443901bbb48289010d2e3342fdeb54577da
hamza03-SE <hamza.erradi.64@edu.uiz.ac.ma>
2025-07-28 23:52:08 +0100

    Ajout des classes modèles Utilisateur et Cours

---
+++ src/main/java/com/coopchal/lms/models/Cours.java
@@ -0,0 +1,41 @@
+package com.coopchal.lms.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "cours")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cours {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titre;
+    private String description;
+    private String imageUrl;
+
+    @ManyToOne
+    @JsonIgnoreProperties({"coursInscrits", "motDePasse"})
+    private Utilisateur formateur;
+
+    @ManyToMany
+    @JoinTable(
+            name = "cours_apprenants",
+            joinColumns = @JoinColumn(name = "cours_id"),
+            inverseJoinColumns = @JoinColumn(name = "apprenant_id")
+    )
+    @JsonIgnore
+    private List<Utilisateur> apprenants;
+
+    @OneToMany(mappedBy = "cours", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<ModuleCours> modules;
+}
